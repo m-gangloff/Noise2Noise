617cbd2c64d42c5e30cf8703d851dfe3833797af
magangloff <71378343+magangloff@users.noreply.github.com>
2022-05-20 15:23:30 +0200

    Added original noise2noise model

--- Proj_SC1_SC2_SC3/Miniproject_1/model.py
+++ Proj_SC1_SC2_SC3/Miniproject_1/model.py
@@ -8,27 +8,27 @@ class Model () :
     def __init__(self) -> None :
         ## instantiate model + optimizer + loss function + any other stuff you need
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = UNet().type(torch.float32).to(self.device)
-        self.eta = 1e-3
-        self.w_decay = 0
-        self.optimizer = optim.SGD(self.model.parameters(), lr=self.eta, weight_decay=self.w_decay)
-        #self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, patience=2, threshold=5, verbose=True)
+        # self.model = UNet().type(torch.float32).to(self.device)
+        # self.model = UNet_Noise2Noise().type(torch.float32).to(self.device)
+        self.model = Noise2Noise_orig().type(torch.float32).to(self.device)
+        self.eta = 1e-7
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.eta)
+        #self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, patience=1, threshold=1e-2, verbose=True)
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=3, gamma=0.1)
         self.criterion = nn.MSELoss()
-        self.batch_size = 50
+        self.batch_size = 4
+        self.losses = []
         print('\n####### Model initialization #########')
         print('\tDevice:\t\t {}'.format(self.device))
         print('\tLearning rate:\t {}'.format(self.eta))
-        print('\tWeight Decay:\t {}'.format(self.w_decay))
         print('\tBatch size:\t {}'.format(self.batch_size))
         print('######################################\n')
-
-        pass
 
     def load_pretrained_model(self, path='bestmodel.pth') -> None :
         ## This loads the parameters saved in bestmodel.pth into the model
         self.model.load_state_dict(torch.load(path))
 
-    def train(self, train_input, train_target, num_epochs) -> None :
+    def train(self, train_input, train_target, num_epochs=9) -> None :
         #: train_input : tensor of size (N, C, H, W) containing a noisy version of the images.
         #: train_target : tensor of size (N, C, H, W) containing another noisy version of the same images, which only differs from the input by their noise. 
 
@@ -40,6 +40,7 @@ class Model () :
                 output = self.model(train_input.narrow(0, batch, self.batch_size).to(self.device))
                 loss = self.criterion(output, train_target.narrow(0, batch, self.batch_size).to(self.device))
                 epoch_loss += loss
+                self.losses.append(loss)
 
                 self.model.zero_grad()
                 loss.backward()
@@ -48,7 +49,8 @@ class Model () :
                 #print('Epoch {}/{},\tloss: {:.4f}'.format(epoch+1, num_epochs, loss))
 
             epoch_loss_avg = epoch_loss / num_batches
-            print('Epoch {}/{},\tloss: {:.4f}'.format(epoch+1, num_epochs, epoch_loss_avg))
+            print('Epoch {}/{},\tloss: {:.4f},\tlr: {:.4f}'.format(epoch+1, num_epochs, epoch_loss_avg, self.optimizer.param_groups[0]['lr']))
+            # self.scheduler.step(epoch_loss_avg)
             #self.scheduler.step()
 
     def predict(self, test_input) -> torch.Tensor :
@@ -57,33 +59,26 @@ class Model () :
 
         # TODO sometimes problem with memory when predicting entire model
         self.model = self.model.to('cpu')
-        output = self.model(test_input)
+        #output = self.model(test_input)
+        output = self.model(test_input/255 - 0.5)
         #output = self.model(test_input.to(self.device))
-        output[output < 0] = 0
-        output[output > 255] = 255
-        return output
+        #output[output < 0] = 0
+        #output[output > 255] = 255
+        output[output < -0.5] = -0.5
+        output[output > 0.5] = 0.5
+        output += 0.5
+
+        return output * 255
 
     def save_model(self, path) -> None:
         #: path : String represting the path with name where the model parameters are saved to.
         torch.save(self.model.state_dict(), path)
 
 
-# https://www.youtube.com/watch?v=IHq1t7NxS8k
-class BlockConvUNet(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(BlockConvUNet, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(inplace=True)
-       )
-
-    def forward(self, x):
-        return self.conv(x)
-
+# UNet: Convolutional Networks for Biomedical Image Segmentation
+# https://arxiv.org/abs/1505.04597
+# https://www.youtube.com/watch?v=oLvmLJkmXuc Paper Walkthrough
+# https://www.youtube.com/watch?v=IHq1t7NxS8k Implementation from scratch
 class UNet(nn.Module):
     def __init__(self, channels=[32, 64, 128, 256]):
         super(UNet, self).__init__()
@@ -98,15 +93,15 @@ class UNet(nn.Module):
         # Down part -> Encoder
         in_channels = 3
         for out_channels in channels:
-            self.downs.append(BlockConvUNet(in_channels, out_channels))
+            self.downs.append(DoubleConv(in_channels, out_channels))
             in_channels = out_channels
 
         # Bottom part
-        self.bottom = BlockConvUNet(channels[-1], channels[-1]*2)
+        self.bottom = DoubleConv(channels[-1], channels[-1]*2)
 
         # Up part -> Decoder
         for out_channels in reversed(channels):
-            self.ups_block_conv.append(BlockConvUNet(in_channels=out_channels*2, out_channels=out_channels))
+            self.ups_block_conv.append(DoubleConv(in_channels=out_channels*2, out_channels=out_channels))
             self.ups_conv2d.append(nn.ConvTranspose2d(
                     in_channels=out_channels*2, out_channels=out_channels, kernel_size=2, stride=2
                 )
@@ -146,10 +141,323 @@ class UNet(nn.Module):
         return x
 
 
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, batchNorm=True):
+        super(DoubleConv, self).__init__()
+        if batchNorm:
+            self.conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(0.1, inplace=True),
+                nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(0.1, inplace=True)
+            )
+        else:
+            self.conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+                nn.LeakyReLU(0.1, inplace=True),
+                nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+                nn.LeakyReLU(0.1, inplace=True)
+            )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class SingleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, batchNorm=True):
+        super(SingleConv, self).__init__()
+
+        if batchNorm:
+            self.conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(0.1, inplace=True)
+            )
+        else:
+            self.conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+                nn.LeakyReLU(0.1, inplace=True)
+            )
+
+    def forward(self, x):
+        return self.conv(x)
+
+# Noise2Noise UNet implementation https://arxiv.org/abs/1803.04189
+# Noise2Noise presentation: https://www.youtube.com/watch?v=dcV0OfxjrPQ
+# https://fleuret.org/dlc/materials/dlc-handout-7-3-denoising-autoencoders.pdf page 18 for UNet layers
+class UNet_Noise2Noise(nn.Module):
+    def __init__(self):
+        super(UNet_Noise2Noise, self).__init__()
+
+        ## Encoder
+        maxPool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        #Enc_Conv0, Enc_Conv1, Pool1
+        doubleConv_down = nn.Sequential(
+            DoubleConv(3, 48),
+            maxPool
+        )
+
+        #Enc_Conv2, Pool2, 3, 4, 5
+        singleConv_down = nn.Sequential(
+            SingleConv(48, 48),
+            maxPool
+        )
+        
+        self.encoder = nn.ModuleList([
+            doubleConv_down,    #Enc_Conv0, Enc_Conv1, Pool1
+            singleConv_down,    #Enc_Conv2, Pool2
+            singleConv_down,    #Enc_Conv3, Pool3
+            singleConv_down,    #Enc_Conv4, Pool4
+            singleConv_down,    #Enc_Conv5, Pool5
+        ])
+
+        #Enc_Conv6, Upsample5
+        self.bottom = nn.Sequential(
+            SingleConv(48, 48),
+            nn.ConvTranspose2d(48, 48, kernel_size=2, stride=2)
+        )
+
+
+        ## Decoder
+        # Upsample5
+        # Since the images are now of dimension 1x1 due to their small size, 
+        # we add padding=1 and use a kernel of size 3 
+        #upsample_bottom = nn.ConvTranspose2d(48, 48, kernel_size=3, stride=1, padding=1)
+
+
+        #Dec_Conv5A, Dec_Conv5B, Upsample4
+        doubleConv_up4 = nn.Sequential(
+            DoubleConv(96, 96),
+            nn.ConvTranspose2d(96, 96, kernel_size=2, stride=2)
+        )
+
+        #Dec_Conv4A, Dec_Conv4B, Upsample3
+        doubleConv_up3 = nn.Sequential(
+            DoubleConv(144, 96),
+            nn.ConvTranspose2d(96, 96, 2, 2)
+        )
+
+        #Dec_Conv3A, Dec_Conv3B, Upsample2
+        doubleConv_up2 = nn.Sequential(
+            DoubleConv(144, 96),
+            nn.ConvTranspose2d(96, 96, 2, 2)
+        )
+
+        #Dec_Conv2A, Dec_Conv2B, Upsample1
+        doubleConv_up1 = nn.Sequential(
+            DoubleConv(144, 96),
+            nn.ConvTranspose2d(96, 96, 2, 2)
+        )
+
+        #Dec_Conv1A, Dec_Conv1B, Dec_Conv1C
+        doubleConv_up0 = nn.Sequential(
+            SingleConv(96+3, 64),
+            SingleConv(64, 32),
+            SingleConv(32, 3)
+        )
+
+        self.decoder = nn.ModuleList([
+            doubleConv_up4,     # Dec_Conv5A, Dec_Conv5B, Upsample4
+            doubleConv_up3,     # Dec_Conv4A, Dec_Conv4B, Upsample3
+            doubleConv_up2,     # Dec_Conv3A, Dec_Conv3B, Upsample2
+            doubleConv_up1,     # Dec_Conv2A, Dec_Conv2B, Upsample1
+            doubleConv_up0      # Dec_Conv1A, Dec_Conv1B, Dec_Conv1C
+        ])
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data) # Using He et al. 2015
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant(m.weight, 1)
+                m.bias.data.zero_()
+
+
+    
+    def forward(self, x):
+        #print(x.shape)
+        downs = []
+        #print('DOWN')
+        # Compute down path and add to list
+        for down in self.encoder:
+            # Append before applying the block to add the input to the skip connections
+            downs.append(x)
+            #print(x.shape)
+            x = down(x)
+
+    
+
+        # Remove last element: bottom block has no skip connection
+        # downs.pop()
+        #print('Bottom')
+        #print(x.shape)
+        x = self.bottom(x)
+        #print(x.shape)
+        x = torch.cat((x, downs.pop()), dim=1)
+        #print(x.shape)
+        #print(len(downs))
+        #print('UP')
+        # Reverse list for skip connections
+        downs.reverse()
+    	  # Compute up path
+        for idx, up in enumerate(self.decoder):
+            #print(idx)
+            x = up(x)
+            #print(x.shape)
+            # Add skip connections except for final block
+            if idx < len(downs):
+                #print('x shape: ', x.shape)
+                #print('down shape: ', downs[idx].shape)
+                x = torch.cat((x, downs[idx]), dim=1)
+            #print(x.shape)
+            
+
+        return x
+
+
+# Noise2Noise UNet implementation https://arxiv.org/abs/1803.04189
+# Noise2Noise presentation: https://www.youtube.com/watch?v=dcV0OfxjrPQ
+# https://fleuret.org/dlc/materials/dlc-handout-7-3-denoising-autoencoders.pdf page 18 for UNet layers
+class Noise2Noise_orig(nn.Module):
+    def __init__(self):
+        super(Noise2Noise_orig, self).__init__()
+
+        ## Encoder
+        maxPool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        #Enc_Conv0, Enc_Conv1, Pool1
+        doubleConv_down = nn.Sequential(
+            DoubleConv(3, 48, batchNorm=False),
+            maxPool
+        )
+
+        #Enc_Conv2, Pool2, 3, 4, 5
+        singleConv_down = nn.Sequential(
+            SingleConv(48, 48, batchNorm=False),
+            maxPool
+        )
+        
+        self.encoder = nn.ModuleList([
+            doubleConv_down,    #Enc_Conv0, Enc_Conv1, Pool1
+            singleConv_down,    #Enc_Conv2, Pool2
+            singleConv_down,    #Enc_Conv3, Pool3
+            singleConv_down,    #Enc_Conv4, Pool4
+            singleConv_down,    #Enc_Conv5, Pool5
+        ])
+
+        #Enc_Conv6, Upsample5
+        self.bottom = nn.Sequential(
+            SingleConv(48, 48, batchNorm=False),
+            nn.ConvTranspose2d(48, 48, kernel_size=2, stride=2)
+        )
+
+
+        ## Decoder
+        #Dec_Conv5A, Dec_Conv5B, Upsample4
+        doubleConv_up4 = nn.Sequential(
+            DoubleConv(96, 96, batchNorm=False),
+            nn.ConvTranspose2d(96, 96, kernel_size=2, stride=2)
+        )
+
+        #Dec_Conv4A, Dec_Conv4B, Upsample3
+        doubleConv_up3 = nn.Sequential(
+            DoubleConv(144, 96, batchNorm=False),
+            nn.ConvTranspose2d(96, 96, 2, 2)
+        )
+
+        #Dec_Conv3A, Dec_Conv3B, Upsample2
+        doubleConv_up2 = nn.Sequential(
+            DoubleConv(144, 96, batchNorm=False),
+            nn.ConvTranspose2d(96, 96, 2, 2)
+        )
+
+        #Dec_Conv2A, Dec_Conv2B, Upsample1
+        doubleConv_up1 = nn.Sequential(
+            DoubleConv(144, 96, batchNorm=False),
+            nn.ConvTranspose2d(96, 96, 2, 2)
+        )
+
+        #Dec_Conv1A, Dec_Conv1B, Dec_Conv1C
+        doubleConv_up0 = nn.Sequential(
+            SingleConv(96+3, 64, batchNorm=False),
+            SingleConv(64, 32, batchNorm=False),
+            SingleConv(32, 3, batchNorm=False)
+        )
+
+        self.decoder = nn.ModuleList([
+            doubleConv_up4,     # Dec_Conv5A, Dec_Conv5B, Upsample4
+            doubleConv_up3,     # Dec_Conv4A, Dec_Conv4B, Upsample3
+            doubleConv_up2,     # Dec_Conv3A, Dec_Conv3B, Upsample2
+            doubleConv_up1,     # Dec_Conv2A, Dec_Conv2B, Upsample1
+            doubleConv_up0      # Dec_Conv1A, Dec_Conv1B, Dec_Conv1C
+        ])
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data) # Using He et al. 2015
+                m.bias.data.zero_()
+
+
+    def forward(self, x):
+        #print(x.shape)
+        downs = []
+        #print('DOWN')
+        # Compute down path and add to list
+        for down in self.encoder:
+            # Append before applying the block to add the input to the skip connections
+            downs.append(x)
+            #print(x.shape)
+            x = down(x)
+
+    
+
+        # Remove last element: bottom block has no skip connection
+        # downs.pop()
+        #print('Bottom')
+        #print(x.shape)
+        x = self.bottom(x)
+        #print(x.shape)
+        x = torch.cat((x, downs.pop()), dim=1)
+        #print(x.shape)
+        #print(len(downs))
+        #print('UP')
+        # Reverse list for skip connections
+        downs.reverse()
+    	  # Compute up path
+        for idx, up in enumerate(self.decoder):
+            #print(idx)
+            x = up(x)
+            #print(x.shape)
+            # Add skip connections except for final block
+            if idx < len(downs):
+                #print('x shape: ', x.shape)
+                #print('down shape: ', downs[idx].shape)
+                x = torch.cat((x, downs[idx]), dim=1)
+            #print(x.shape)
+            
+
+        return x
+
+
 def test():
     x = torch.randn((10, 3, 32, 32))
     model = UNet()
     preds = model(x)    
+    assert preds.shape == x.shape
+
+    model = UNet_Noise2Noise()
+    preds = model(x)
     assert preds.shape == x.shape
 
 if False:
--- Proj_SC1_SC2_SC3/Miniproject_1/others/run.py
+++ Proj_SC1_SC2_SC3/Miniproject_1/others/run.py
@@ -5,12 +5,39 @@ import time
 import sys
 sys.path.append('../')
 from model import Model
+# Import tqdm if installed
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = lambda x: x
+
 
 
 def psnr(denoised, ground_truth):
     # Peak Signal to Noise Ratio : denoised and ground_truth have range [0, 1]
     mse = torch.mean((denoised.to(device) - ground_truth.to(device)) ** 2)
     return -10 * torch.log10(mse + 10**-8)
+
+# From test.py
+def compute_psnr(x, y, max_range=1.0):
+    assert x.shape == y.shape and x.ndim == 4
+    return 20 * torch.log10(torch.tensor(max_range)) - 10 * torch.log10(((x-y) ** 2).mean((1,2,3))).mean()
+
+# Adapted from test.py
+def _test_model_pnsr(model, val_input, val_target):
+    val_input = val_input.float()
+    val_target = val_target.float() / 255.0
+    
+    mini_batch_size = 100
+    model_outputs = []
+    for b in tqdm(range(0, val_input.size(0), mini_batch_size)):
+        output = model.predict(val_input.narrow(0, b, mini_batch_size))
+        model_outputs.append(output.cpu())
+    model_outputs = torch.cat(model_outputs, dim=0) / 255.0
+
+    output_psnr = compute_psnr(model_outputs, val_target)
+    print(f"[PSNR : {output_psnr:.2f} dB]")
+
 
 
 def normalize_tensor_0_1(tensor):
@@ -47,79 +74,74 @@ def unnormalize_images(tensor):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def data_augmentation(train, target):
+    # Flip rgb and bgr
+    train = torch.cat((train, train.flip([2])), dim=0)
+    target = torch.cat((target, target.flip([2])), dim=0)
+
+    return train, target
+
 # Place train and val data at the root of the repo
-train_data_path = '../../../train_data.pkl'
-val_data_path = '../../../val_data.pkl'
+def train(train_model=True, normalize_data=False, augment_data=False, num_epochs=9, train_data_path = '../../../train_data.pkl', val_data_path = '../../../val_data.pkl'):
+    #print('s')
+    noisy_imgs_1, noisy_imgs_2 = torch.load(train_data_path)
+    noisy_imgs_val, clean_imgs_val = torch.load(val_data_path)
 
-noisy_imgs_1, noisy_imgs_2 = torch.load(train_data_path)
-noisy_imgs_val, clean_imgs_val = torch.load(val_data_path)
+    noisy_imgs_1 = noisy_imgs_1.type(torch.float32)
+    noisy_imgs_2 = noisy_imgs_2.type(torch.float32)
 
+    noisy_imgs_val = noisy_imgs_val.type(torch.float32)
 
-noisy_imgs_1 = noisy_imgs_1.type(torch.float32)
-# noisy_imgs_1 = normalize_tensor_0_1(noisy_imgs_1)
-#noisy_imgs_1 = normalize_tensor(noisy_imgs_1)
-# noisy_imgs_1 = normalize_images(noisy_imgs_1)
-# mu_noisy_imgs1, std_noisy_imgs1 = noisy_imgs_1.mean(), noisy_imgs_1.std()
-# noisy_imgs_1.sub_(mu_noisy_imgs1).div_(std_noisy_imgs1)
+    if normalize_data:
+      noisy_imgs_1 = noisy_imgs_1/255 - 0.5
+      noisy_imgs_2 = noisy_imgs_2/255 - 0.5
 
-noisy_imgs_2 = noisy_imgs_2.type(torch.float32)
-# noisy_imgs_2 = normalize_tensor_0_1(noisy_imgs_2)
-#noisy_imgs_2 = normalize_tensor(noisy_imgs_2)
-# noisy_imgs_2 = normalize_images(noisy_imgs_2)
-# mu_noisy_imgs2, std_noisy_imgs2 = noisy_imgs_2.mean(), noisy_imgs_2.std()
-# noisy_imgs_2.sub_(mu_noisy_imgs2).div_(std_noisy_imgs2)
+    if augment_data:
+      noisy_imgs_1, noisy_imgs_2 = data_augmentation(noisy_imgs_1, noisy_imgs_2)
 
-noisy_imgs_val = noisy_imgs_val.type(torch.float32)
-# noisy_imgs_val = normalize_tensor_0_1(noisy_imgs_val)
-#noisy_imgs_val = normalize_tensor(noisy_imgs_val)
-# noisy_imgs_val = normalize_images(noisy_imgs_val)
-# mu_noisy_imgs_val, std_noisy_imgs_val = noisy_imgs_val.mean(), noisy_imgs_val.std()
-# noisy_imgs_val.sub_(mu_noisy_imgs_val).div_(std_noisy_imgs_val)
+    print(noisy_imgs_1.shape)
 
+    num_epochs = 3
 
-num_epochs = 8
+    model = Model()
 
-train = True
-#train = False
+    if train_model:
+        t_start = time.time()
+        model.train(noisy_imgs_1, noisy_imgs_2, num_epochs)
+        t_end = time.time()
+        print('\n#################################')
+        print('\nTraining finished in {:.1f} minutes'.format((t_end-t_start)/60))
+        print('\n#################################\n')
+        model.save_model(path='./models/noise2noise_orig.pth')
+    else:
+        #model.load_pretrained_model(path='../bestmodel.pth')
+        model.load_pretrained_model(path='./models/noise2noise_orig.pth')
 
-model = Model()
+    _test_model_pnsr(model, noisy_imgs_val, clean_imgs_val)
+    denoised = model.predict(noisy_imgs_val) / 255
 
-if train:
-    t_start = time.time()
-    model.train(noisy_imgs_1, noisy_imgs_2, num_epochs)
-    t_end = time.time()
-    print('\n#################################')
-    print('\nTraining finished in {:.1f} minutes'.format((t_end-t_start)/60))
-    print('\n#################################\n')
+    print(denoised.max())
+    print(denoised.min())
 
-    model.save_model(path='../bestmodel.pth')
-else:
-    model.load_pretrained_model(path='../bestmodel.pth')
+    clean_imgs_val = clean_imgs_val.type(torch.float32).to(device)
+    clean_imgs_val /= 255
 
-denoised = model.predict(noisy_imgs_val) / 255
+    noisy_imgs_val /= 255
 
-print(denoised.max())
-print(denoised.min())
+    rows, cols = 5, 3
+    H, C, W = 3, 32, 32
+    row_start = random.randint(0, noisy_imgs_val.shape[0])
 
-clean_imgs_val = clean_imgs_val.type(torch.float32).to(device)
-clean_imgs_val /= 255
+    f, ax = plt.subplots(rows, cols)
+    for row in range(row_start, row_start+rows):
+        noisy_img = noisy_imgs_val[row].view(C, W, H).cpu().detach().numpy()
+        denoised_img = denoised[row].view(C, W, H).cpu().detach().numpy()
+        clear_img = clean_imgs_val[row].view(C, W, H).cpu().detach().numpy()
 
-noisy_imgs_val /= 255
+        ax[row-row_start, 0].imshow(noisy_img)
+        ax[row-row_start, 1].imshow(denoised_img)
+        ax[row-row_start, 2].imshow(clear_img)
+    plt.show()
 
-rows, cols = 5, 3
-H, C, W = 3, 32, 32
-row_start = random.randint(0, noisy_imgs_val.shape[0])
-
-f, ax = plt.subplots(rows, cols)
-for row in range(row_start, row_start+rows):
-    noisy_img = noisy_imgs_val[row].view(C, W, H).cpu().detach().numpy()
-    denoised_img = denoised[row].view(C, W, H).cpu().detach().numpy()
-    clear_img = clean_imgs_val[row].view(C, W, H).cpu().detach().numpy()
-
-    ax[row-row_start, 0].imshow(noisy_img)
-    ax[row-row_start, 1].imshow(denoised_img)
-    ax[row-row_start, 2].imshow(clear_img)
-plt.show()
-
-print('\nPeak Signal-to-Noise Ratio base:\t{:.4f}\n'.format(psnr(noisy_imgs_val, clean_imgs_val)))
-print('Peak Signal-to-Noise Ratio:\t{:.4f}\n'.format(psnr(denoised, clean_imgs_val)))
+    print('\nPeak Signal-to-Noise Ratio base:\t{:.4f}\n'.format(psnr(noisy_imgs_val, clean_imgs_val)))
+    print('Peak Signal-to-Noise Ratio:\t{:.4f}\n'.format(psnr(denoised, clean_imgs_val)))
